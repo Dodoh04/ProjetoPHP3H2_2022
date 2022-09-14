5b9eb186c75ec6b256e2aad60dc9956019fcda06
KeilaSuellen <keilasuellen02@gmail.com>
2022-09-14 19:42:08 -0300

    function

---
+++ icnludePhp/corpo.php
@@ -0,0 +1,7 @@
+<?php
+echo"<hr/>";
+echo "<h1>Estamos aqui tentando versionar fazendo um pull</h1>";
+echo "<p>E não está dando certo </p>";
+echo "<p>E aqui seria outro texto qualquer</p>";
+echo"<hr/>";
+?>
---
+++ icnludePhp/pagina.php
@@ -0,0 +1,11 @@
+
+<?php 
+    #incluindo o cabeçalho da pagina
+    include("cabecalho.php");
+
+    #incluindo o corpo da pagina
+    include("corpo.php");
+
+    #incluindo o rodape da pagina
+    include("rodape.php");
+?>
