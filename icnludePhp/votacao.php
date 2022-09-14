7c3b7035b283ff4e37f27dbc553073f678edd53a
KeilaSuellen <keilasuellen02@gmail.com>
2022-09-14 20:23:24 -0300

    tentativa de html

--- icnludePhp/votacao.php
+++ icnludePhp/votacao.php
@@ -25,9 +25,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Votação</title>
+    <title>Votação</title>   
 </head>
 <body>
     <h1>Condição de votação</h1>
+
+
+    <p staly:color:#fff;;> <color><div id="ffffff"></div><color/>  Quando for adicionar php no html, não se salva o arquivo HTML e sim como PHP!!!</p>
 </body>
 </html>
